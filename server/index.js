537ac8773f4aec9f0b797a3d045735f2cea50895
Dheeraj Soni <dheerajsoni7023@gmail.com>
2024-01-08 18:54:09 +0530

    establish database connection and created the router and server

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");           //to connet with frontend
+const mongoose = require("mongoose");
+
+const userRoute = require("./Routes/userRoute");
+
+const app = express();
+require("dotenv").config();         //to access the environment variable
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute);
+
+
+// CRUD
+// app.post to create data
+// app.get to  read data
+// app.put to update data
+// app.delete to delete data
+
+app.get("/", (req, res) =>{
+    res.send("Welcome to my Chat app APIs.....");
+});
+
+const port = 8080;                      //for testing
+// const port = process.env.port;      //for deployment
+
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port....: ${port}`);
+});
+
+//connet to database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB: Successfully established connection");
+}).catch((error) => {
+    console.log("MongoDB connection failed: ", error.message);
+});
