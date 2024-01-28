c486bbd60bd6b1023e141fea924ca9b513e13e06
Dheeraj Soni <dheerajsoni7023@gmail.com>
2024-01-28 23:34:10 +0530

    add the register and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
-import{Routes, Route, Navigate} from 'react-router-dom'
-import Chat from './pages/chat';
-import Login from './pages/Login';
-import Register from './pages/Register';
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./pages/chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav } from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <>
-  <Routes>
-    <Route path="/" element={<Chat/>}></Route>
-    <Route path="/login" element={<Login/>}></Route>
-    <Route path="/register" element={<Register/>}></Route>
-    <Route path="*" element={<Navigate to="/"/>}></Route>
-  </Routes>
-
-
-
-  </>;
+  return (
+    <>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Chat />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="*" element={<Navigate to="/" />}></Route>
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
