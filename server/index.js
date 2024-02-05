49568faed759b7c14866fbf68a577434821a9a56
Dheeraj Soni <dheerajsoni7023@gmail.com>
2024-02-05 17:28:42 +0530

    add the message model and api

--- server/index.js
+++ server/index.js
@@ -1,43 +1,40 @@
 const express = require("express");
-const cors = require("cors");           //to connet with frontend
+const cors = require("cors"); //to connet with frontend
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute");
 const chatRoute = require("./Routes/chatRoute");
+const messageRoute = require("./Routes/messageRoute");
 
 const app = express();
-require("dotenv").config();         //to access the environment variable
+require("dotenv").config(); //to access the environment variable
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoute);
 app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
-
-// CRUD
-// app.post to create data
-// app.get to  read data
-// app.put to update data
-// app.delete to delete data
-
-app.get("/", (req, res) =>{
-    res.send("Welcome to my Chat app APIs.....");
+app.get("/", (req, res) => {
+  res.send("Welcome to my Chat app APIs.....");
 });
 
-const port = 8080;                      //for testing
+const port = 8080; //for testing
 // const port = process.env.port;      //for deployment
 
-
 app.listen(port, (req, res) => {
-    console.log(`Server running on port....: ${port}`);
+  console.log(`Server running on port....: ${port}`);
 });
 
 //connet to database
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("MongoDB: Successfully established connection");
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log("MongoDB connection failed: ", error.message);
-});
+  });
